ec8713655de343242f04af8aa601dcde63bd2f36
Lautian <gtenholt@gmail.com>
2026-03-17 12:45:39 +0000

    Add moderator user role separate from staff-status

--- moderation/utils.py
+++ moderation/utils.py
@@ -1,3 +1,12 @@
+MODERATOR_GROUP_NAME = "Moderators"
+
+
 def is_moderator(user):
-    """Return True if the user has moderator privileges (staff or superuser)."""
-    return user.is_active and (user.is_staff or user.is_superuser)
+    """Return True if the user has moderator privileges."""
+    if not user.is_authenticated or not user.is_active:
+        return False
+    return (
+        user.is_staff
+        or user.is_superuser
+        or user.groups.filter(name=MODERATOR_GROUP_NAME).exists()
+    )
--- users/admin.py
+++ users/admin.py
@@ -1,1 +1,55 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.contrib.admin.sites import NotRegistered
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth.models import Group
+
+from moderation.utils import MODERATOR_GROUP_NAME
+
+User = get_user_model()
+
+
+@admin.action(description="Assign selected users to Moderators group")
+def assign_moderator_role(modeladmin, request, queryset):
+    moderator_group, _ = Group.objects.get_or_create(name=MODERATOR_GROUP_NAME)
+    assigned_count = 0
+    for user in queryset:
+        if not user.groups.filter(pk=moderator_group.pk).exists():
+            user.groups.add(moderator_group)
+            assigned_count += 1
+    messages.success(request, f"Assigned moderator role to {assigned_count} user(s).")
+
+
+@admin.action(description="Remove selected users from Moderators group")
+def remove_moderator_role(modeladmin, request, queryset):
+    moderator_group = Group.objects.filter(name=MODERATOR_GROUP_NAME).first()
+    if moderator_group is None:
+        messages.info(request, "Moderators group does not exist yet.")
+        return
+
+    removed_count = 0
+    for user in queryset:
+        if user.groups.filter(pk=moderator_group.pk).exists():
+            user.groups.remove(moderator_group)
+            removed_count += 1
+    messages.success(request, f"Removed moderator role from {removed_count} user(s).")
+
+
+class UserAdmin(DjangoUserAdmin):
+    list_display = DjangoUserAdmin.list_display + ("has_moderator_role",)
+    actions = (DjangoUserAdmin.actions or []) + [
+        assign_moderator_role,
+        remove_moderator_role,
+    ]
+
+    @admin.display(boolean=True, description="Moderator")
+    def has_moderator_role(self, obj):
+        return obj.groups.filter(name=MODERATOR_GROUP_NAME).exists()
+
+
+try:
+    admin.site.unregister(User)
+except NotRegistered:
+    pass
+
+admin.site.register(User, UserAdmin)
